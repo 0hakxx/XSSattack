546b04df388a22866736fbd887170aaac0afd2e2
yeong <yeonghak2207@gmail.com>
2024-12-22 17:57:56 +0900

    Third pUSH

--- board/action.php
+++ board/action.php
@@ -1,7 +1,3 @@
-<?
-	@session_start();
-	header("Content-Type: text/html; charset=UTF-8");
-	include("./common.php");
 <?php
 // 세션 시작
 @session_start();
@@ -10,9 +6,6 @@ header("Content-Type: text/html; charset=UTF-8");
 // common.php 파일 포함 (데이터베이스 연결 등의 공통 기능 포함)
 include("./common.php");
 
-	$mode = $_REQUEST["mode"];
-	$db_conn = mysql_conn();
-	$id = $db_conn->real_escape_string($_SESSION["id"]);
 // GET 또는 POST로 전달된 'mode' 파라미터 값을 가져옴
 $mode = $_REQUEST["mode"];
 // 데이터베이스 연결
@@ -20,15 +13,6 @@ $db_conn = mysql_conn();
 // 세션에서 사용자 ID를 가져와 SQL 인젝션 방지를 위해 이스케이프 처리
 $id = $db_conn->real_escape_string($_SESSION["id"]);
 
-	if(empty($id)) {
-		echo "<script>alert('올바른 접근이 아닙니다.');history.back(-1);</script>";
-		exit();
-	}
-	
-	if($mode == "write") {
-		$title = $db_conn->real_escape_string($_POST["title"]);
-		$writer = $db_conn->real_escape_string($_SESSION["name"]);
-		$content = $db_conn->real_escape_string($_POST["content"]);
 // 사용자 ID가 없으면 (로그인하지 않은 상태) 오류 메시지 출력 후 이전 페이지로 이동
 if(empty($id)) {
     echo "<script>alert('올바른 접근이 아닙니다.');history.back(-1);</script>";
@@ -43,24 +27,12 @@ if($mode == "write") {
     $writer = $db_conn->real_escape_string($_SESSION["name"]);
     $content = $db_conn->real_escape_string($_POST["content"]);
 
-		if(empty($title) || empty($content)) {
-			echo "<script>alert('빈칸이 존재합니다.');history.back(-1);</script>";
-			exit();
-		}
     // 제목이나 내용이 비어있으면 오류 메시지 출력 후 이전 페이지로 이동
     if(empty($title) || empty($content)) {
         echo "<script>alert('빈칸이 존재합니다.');history.back(-1);</script>";
         exit();
     }
 
-		$content = str_replace("\\r\\n", "<br>", $content);
-		
-		$query = "insert into {$tb_name}(title, id, writer, content, regdate) values('{$title}', '{$id}', '{$writer}', '{$content}', now())";
-		$db_conn->query($query);
-	} else if($mode == "modify") {
-		$idx = $_POST["idx"];
-		$title = $db_conn->real_escape_string($_POST["title"]);
-		$content = $db_conn->real_escape_string($_POST["content"]);
     // 줄바꿈 문자를 HTML <br> 태그로 변환
     $content = str_replace("\\r\\n", "<br>", $content);
 
@@ -75,50 +47,29 @@ if($mode == "write") {
     $title = $db_conn->real_escape_string($_POST["title"]);
     $content = $db_conn->real_escape_string($_POST["content"]);
 
-		if(empty($idx) || empty($title) || empty($content)) {
-			echo "<script>alert('빈칸이 존재합니다.');history.back(-1);</script>";
-			exit();
-		}
     // 글 번호, 제목, 내용 중 하나라도 비어있으면 오류 메시지 출력 후 이전 페이지로 이동
     if(empty($idx) || empty($title) || empty($content)) {
         echo "<script>alert('빈칸이 존재합니다.');history.back(-1);</script>";
         exit();
     }
 
-		if(!is_numeric($idx)) {
-			echo "<script>alert('입력 값이 잘못되었습니다.');history.back(-1);</script>";
-			exit();
-		}
     // 글 번호가 숫자가 아니면 오류 메시지 출력 후 이전 페이지로 이동
     if(!is_numeric($idx)) {
         echo "<script>alert('입력 값이 잘못되었습니다.');history.back(-1);</script>";
         exit();
     }
 
-		$query = "select * from {$tb_name} where idx={$idx} and id='{$id}'";
-		$result = $db_conn->query($query);
-		$num = $result->num_rows;
     // 해당 글이 현재 로그인한 사용자의 것인지 확인
     $query = "select * from {$tb_name} where idx={$idx} and id='{$id}'";
     $result = $db_conn->query($query);
     $num = $result->num_rows;
 
-		if($num == 0) {
-			echo "<script>alert('올바른 접근이 아닙니다.');history.back(-1);</script>";
-			exit();
-		}
     // 해당하는 글이 없으면 오류 메시지 출력 후 이전 페이지로 이동
     if($num == 0) {
         echo "<script>alert('올바른 접근이 아닙니다.');history.back(-1);</script>";
         exit();
     }
 
-		$content = str_replace("\\r\\n", "<br>", $content);
-		
-		$query = "update {$tb_name} set title='{$title}', content='{$content}', regdate=now() where idx={$idx}";
-		$db_conn->query($query);
-	} else if($mode == "delete") {
-		$idx = $_GET["idx"];
     // 줄바꿈 문자를 HTML <br> 태그로 변환
     $content = str_replace("\\r\\n", "<br>", $content);
 
@@ -130,14 +81,6 @@ if($mode == "write") {
     // GET으로 전달된 글 번호를 가져옴
     $idx = $_GET["idx"];
 
-		if(!is_numeric($idx)) {
-			echo "<script>alert('입력 값이 잘못되었습니다.');history.back(-1);</script>";
-			exit();
-		}
-		
-		$query = "select * from {$tb_name} where idx={$idx} and id='{$id}'";
-		$result = $db_conn->query($query);
-		$num = $result->num_rows;
     // 글 번호가 숫자가 아니면 오류 메시지 출력 후 이전 페이지로 이동
     if(!is_numeric($idx)) {
         echo "<script>alert('입력 값이 잘못되었습니다.');history.back(-1);</script>";
@@ -149,14 +92,6 @@ if($mode == "write") {
     $result = $db_conn->query($query);
     $num = $result->num_rows;
 
-		if($num == 0) {
-			echo "<script>alert('올바른 접근이 아닙니다.');history.back(-1);</script>";
-			exit();
-		}
-		
-		$query = "delete from {$tb_name} where idx={$idx}";
-		$db_conn->query($query);
-	}
     // 해당하는 글이 없으면 오류 메시지 출력 후 이전 페이지로 이동
     if($num == 0) {
         echo "<script>alert('올바른 접근이 아닙니다.');history.back(-1);</script>";
@@ -168,8 +103,6 @@ if($mode == "write") {
     $db_conn->query($query);
 }
 
-	echo "<script>location.href='index.php?page=board';</script>";
-	$db_conn->close();
 // 모든 작업이 완료되면 게시판 페이지로 리다이렉트
 echo "<script>location.href='index.php?page=board';</script>";
 // 데이터베이스 연결 종료
